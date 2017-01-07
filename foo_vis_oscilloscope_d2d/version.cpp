df1c65927fa1128c10af9c2473ddca52b1b0acb9
Holger Stenger <holger.stenger@gmx.de>
2017-01-07 13:50:31 +0100

    Add contributor section to about message

--- foo_vis_oscilloscope_d2d/version.cpp
+++ foo_vis_oscilloscope_d2d/version.cpp
@@ -10,6 +10,8 @@ DECLARE_COMPONENT_VERSION(
     "Oscilloscope visualization with support for hardware accelerated graphics using Direct2D\n"
     "\n"
     "Copyright (C) " COMPONENT_COPYRIGHT_YEAR " " COMPONENT_AUTHOR "\n"
+    "Contributors:\n"
+    "  Christopher Snowhill\n"
 )
 
 VALIDATE_COMPONENT_FILENAME(COMPONENT_FILENAME)
